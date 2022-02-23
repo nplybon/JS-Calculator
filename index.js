4a903169f2a72f773412bc149c53ec8127e34199
Nic Plybon <79996570+nplybon@users.noreply.github.com>
2022-02-23 21:45:12 +0000

    needs css

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 var numOne = document.getElementById('num-one');
 var numTwo = document.getElementById('num-two');
-var addSum = document.getElementById('add-sum');
-numOne.addEventListener('input', add);
-numTwo.addEventListener('input', add);
+var result = document.getElementById('result');
+
+document.getElementById("add").addEventListener('click', add);
+document.getElementById("sub").addEventListener('click', sub);
+document.getElementById("mult").addEventListener('click', mult);
+document.getElementById("div").addEventListener('click', div);
+
 function add() {
-var one = parseFloat(numOne.value) || 0;
-var two = parseFloat(numTwo.value) || 0;
-addSum.innerText = one+two;
+    var one = parseFloat(numOne.value) || 0;
+    var two = parseFloat(numTwo.value) || 0;
+    result.innerText = one + two;
 }
+
+function sub() {
+    var one = parseFloat(numOne.value) || 0;
+    var two = parseFloat(numTwo.value) || 0;
+    result.innerText = one - two;
+}
+function mult() {
+    var one = parseFloat(numOne.value) || 0;
+    var two = parseFloat(numTwo.value) || 0;
+    result.innerText = one * two;
+}
+function div() {
+    var one = parseFloat(numOne.value) || 0;
+    var two = parseFloat(numTwo.value) || 0;
+    result.innerText = one / two;
+}
+
